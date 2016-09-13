e1b803f1f85c606a4b74c3c33f2d12918efe3461
Michael Raimi <mar2260@columbia.edu>
2016-09-12 21:32:24 -0400

    Add more logging

--- generate.py
+++ generate.py
@@ -33,63 +33,74 @@ except botocore.exceptions.ClientError as e:
 if exists:
     log(lg, 'bucket: %s found ' % buck_name + "\n")
 else:
+    log(lg, 'Error! Bucket: %s not found ' % buck_name)
+    lg.close()
     sys.exit('Error! Bucket: %s not found ' % buck_name)
 
-for i in xrange(1,8):
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    week = 'Week' + str(i)
-    log(lg, 'Starting week: ' + week + '...' + "\n")
-
-    try:
-        s3obj = s3.Object(buck_name, week)
-    except Exception:
-        sys.exit('Failed to create s3 object. Check your bucket name.')
-
-    for day in days:
-        expanded_file = None
+try:
+    for i in xrange(1,8):
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        week = 'Week' + str(i)
+        log(lg, 'Starting week: ' + week + '...' + "\n")
 
         try:
-            expanded_file = open('/home/ec2-user/data/' + week + '-' + day, 'a')
-        except IOError:
-            sys.exit('Could not create a file to generate data to.')
+            s3obj = s3.Object(buck_name, week)
+        except Exception:
+            log(lg, 'Failed to create s3 object. Check your bucket name.')
+            lg.close()
+            sys.exit('Failed to create s3 object. Check your bucket name.')
 
-        log(lg, 'Starting day: ' + day + '...' + "\n")
-        dir = '/home/ec2-user/' + week + '/' + day + '/gureKddcup-matched.list'
+        for day in days:
+            expanded_file = None
 
-        try:
-            records = open(dir, 'r')
-        except IOError:
-            log(lg, "file at: \n\t" + dir + "\n not found \n")
-            continue
-        for record in records:
-            random.seed()
+            try:
+                expanded_file = open('/home/ec2-user/data/' + week + '-' + day, 'a')
+            except IOError:
+                log(lg, 'Could not create a file to generate data to.')
+                lg.close()
+                sys.exit('Could not create a file to generate data to.')
 
-            for j in xrange(0, dupl_factor):
-                spl = record.strip().split(' ')
-                to_send = ''
+            log(lg, 'Starting day: ' + day + '...' + "\n")
+            dir = '/home/ec2-user/' + week + '/' + day + '/gureKddcup-matched.list'
 
-                # Gently perturb data with small modifications
-                try:
-                    for k in xrange(30, 47):
-                        if k == 37 or k == 38:
-                            spl[k] = max(0, int(spl[k])+random.randint(-1,1))
+            try:
+                records = open(dir, 'r')
+            except IOError:
+                log(lg, "file at: \n\t" + dir + "\n not found \n")
+                continue
+            for record in records:
+                random.seed()
+
+                for j in xrange(0, dupl_factor):
+                    spl = record.strip().split(' ')
+                    to_send = ''
+
+                    # Gently perturb data with small modifications
+                    try:
+                        for k in xrange(30, 47):
+                            if k == 37 or k == 38:
+                                spl[k] = max(0, int(spl[k])+random.randint(-1,1))
+                            else:
+                                spl[k] = float(spl[k]) + float(random.randint(0, 9))/1000000.0
+                    except Exception:
+                        log(lg, 'Malformed record aborted.' + "\n")
+                        continue
+
+                    for field in spl:
+                        if isinstance(field, float):
+                            to_send += ' ' + "%.6f" % field
                         else:
-                            spl[k] = float(spl[k]) + float(random.randint(0, 9))/1000000.0
-                except Exception:
-                    log(lg, 'Malformed record aborted.' + "\n")
-                    continue
+                            to_send = to_send + ' ' + str(field)
 
-                for field in spl:
-                    if isinstance(field, float):
-                        to_send += ' ' + "%.6f" % field
-                    else:
-                        to_send = to_send + ' ' + str(field)
-
-                to_send = to_send.strip()
-                to_send += "\n"
-                expanded_file.write(to_send)
-        expanded_file.close()
-        log(lg, 'Ending day: ' + day + '...' + "\n")
-        s3obj.put(Body=open('/home/ec2-user/data/' + week + '-' + day, 'r'))
-    log(lg, 'Ending week: ' + week + '...' + "\n")
+                    to_send = to_send.strip()
+                    to_send += "\n"
+                    expanded_file.write(to_send)
+            expanded_file.close()
+            log(lg, 'Ending day: ' + day + '...' + "\n")
+            s3obj.put(Body=open('/home/ec2-user/data/' + week + '-' + day, 'r'))
+            log(lg, 'Sent ' + week + '-' + day + ' to s3.')
+        log(lg, 'Ending week: ' + week + '...' + "\n")
+except Exception:
+    log(lg, 'Exception caught at top level. Exiting.')
+    lg.close()
 lg.close()
