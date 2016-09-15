f50cc2c5680d794c719799b66c05dbb243c3fd15
Michael Raimi <mar2260@columbia.edu>
2016-09-15 13:40:24 -0400

    Comment out streaming k means file

--- src/main/scala/streamingKMeans.scala
+++ src/main/scala/streamingKMeans.scala
@@ -50,34 +50,34 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
   * Anytime you add text files to `testDir`, you'll see predicted labels using the current model.
   *
   */
-object StreamingKMeansExample {
-
-  def main(args: Array[String]) {
-    if (args.length != 5) {
-      System.err.println(
-        "Usage: StreamingKMeansExample " +
-          "<trainingDir> <testDir> <batchDuration> <numClusters> <numDimensions>")
-      System.exit(1)
-    }
-
-    // $example on$
-    val conf = new SparkConf().setAppName("StreamingKMeansExample")
-    val ssc = new StreamingContext(conf, Seconds(args(2).toLong))
-
-    val trainingData = ssc.textFileStream(args(0)).map(Vectors.parse)
-    val testData = ssc.textFileStream(args(1)).map(LabeledPoint.parse)
-
-    val model = new StreamingKMeans()
-      .setK(args(3).toInt)
-      .setDecayFactor(1.0)
-      .setRandomCenters(args(4).toInt, 0.0)
-
-    model.trainOn(trainingData)
-    model.predictOnValues(testData.map(lp => (lp.label, lp.features))).print()
-
-    ssc.start()
-    ssc.awaitTermination()
-    // $example off$
-  }
-}
+//object StreamingKMeansExample {
+//
+//  def main(args: Array[String]) {
+//    if (args.length != 5) {
+//      System.err.println(
+//        "Usage: StreamingKMeansExample " +
+//          "<trainingDir> <testDir> <batchDuration> <numClusters> <numDimensions>")
+//      System.exit(1)
+//    }
+//
+//    // $example on$
+//    val conf = new SparkConf().setAppName("StreamingKMeansExample")
+//    val ssc = new StreamingContext(conf, Seconds(args(2).toLong))
+//
+//    val trainingData = ssc.textFileStream(args(0)).map(Vectors.parse)
+//    val testData = ssc.textFileStream(args(1)).map(LabeledPoint.parse)
+//
+//    val model = new StreamingKMeans()
+//      .setK(args(3).toInt)
+//      .setDecayFactor(1.0)
+//      .setRandomCenters(args(4).toInt, 0.0)
+//
+//    model.trainOn(trainingData)
+//    model.predictOnValues(testData.map(lp => (lp.label, lp.features))).print()
+//
+//    ssc.start()
+//    ssc.awaitTermination()
+//    // $example off$
+//  }
+//}
 // scalastyle:on println
