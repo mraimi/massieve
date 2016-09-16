c869caec70b728a1e7cc47dc8ff8a6ebdd68c715
Michael Raimi <mar2260@columbia.edu>
2016-09-16 11:36:49 -0400

    Comment out streaming stuff

--- src/main/scala/streamingKMeans.scala
+++ src/main/scala/streamingKMeans.scala
@@ -16,14 +16,14 @@
  */
 
 // scalastyle:off println
-package org.apache.spark.examples.mllib
-
-import org.apache.spark.SparkConf
-// $example on$
-import org.apache.spark.mllib.clustering.StreamingKMeans
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+//package org.apache.spark.examples.mllib
+//
+//import org.apache.spark.SparkConf
+//// $example on$
+//import org.apache.spark.mllib.clustering.StreamingKMeans
+//import org.apache.spark.mllib.linalg.Vectors
+//import org.apache.spark.mllib.regression.LabeledPoint
+//import org.apache.spark.streaming.{Seconds, StreamingContext}
 // $example off$
 
 /**
@@ -50,37 +50,37 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
   * Anytime you add text files to `testDir`, you'll see predicted labels using the current model.
   *
   */
-object StreamingKMeansExample {
-
-  def main(args: Array[String]) {
-    if (args.length != 5) {
-      System.err.println(
-        "Usage: StreamingKMeansExample " +
-          "<trainingDir> <testDir> <batchDuration> <numClusters> <numDimensions>")
-      System.exit(1)
-    }
-
-    // $example on$
-    val conf = new SparkConf().setAppName("StreamingKMeansExample")
-    val ssc = new StreamingContext(conf, Seconds(args(2).toLong))
-
-    val trainingData = ssc.textFileStream(args(0)).map(Vectors.parse)
-
-    // Get training data into appropriate formpw
-
-    val testData = ssc.textFileStream(args(1)).map(LabeledPoint.parse)
-
-    val model = new StreamingKMeans()
-      .setK(args(3).toInt)
-      .setDecayFactor(1.0)
-      .setRandomCenters(args(4).toInt, 0.0)
-
-    model.trainOn(trainingData)
-    model.predictOnValues(testData.map(lp => (lp.label, lp.features))).print()
-
-    ssc.start()
-    ssc.awaitTermination()
-    // $example off$
-  }
-}
+//object StreamingKMeansExample {
+//
+//  def main(args: Array[String]) {
+//    if (args.length != 5) {
+//      System.err.println(
+//        "Usage: StreamingKMeansExample " +
+//          "<trainingDir> <testDir> <batchDuration> <numClusters> <numDimensions>")
+//      System.exit(1)
+//    }
+//
+//    // $example on$
+//    val conf = new SparkConf().setAppName("StreamingKMeansExample")
+//    val ssc = new StreamingContext(conf, Seconds(args(2).toLong))
+//
+//    val trainingData = ssc.textFileStream(args(0)).map(Vectors.parse)
+//
+//    // Get training data into appropriate formpw
+//
+//    val testData = ssc.textFileStream(args(1)).map(LabeledPoint.parse)
+//
+//    val model = new StreamingKMeans()
+//      .setK(args(3).toInt)
+//      .setDecayFactor(1.0)
+//      .setRandomCenters(args(4).toInt, 0.0)
+//
+//    model.trainOn(trainingData)
+//    model.predictOnValues(testData.map(lp => (lp.label, lp.features))).print()
+//
+//    ssc.start()
+//    ssc.awaitTermination()
+//    // $example off$
+//  }
+//}
 // scalastyle:on println
