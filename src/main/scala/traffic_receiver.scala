f0702c9bf04898f77acb36918857aea1386515fe
Michael Raimi <mar2260@columbia.edu>
2016-09-19 18:49:51 -0400

    Try direct stream to HDFS

--- src/main/scala/traffic_receiver.scala
+++ src/main/scala/traffic_receiver.scala
@@ -7,7 +7,6 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import org.apache.spark.sql._
 import java.io._
-//import kmeans.KMeansObj
 
 object TrafficDataStreaming {
   def main(args: Array[String]) {
@@ -18,16 +17,16 @@ object TrafficDataStreaming {
 
     // Create context with 2 second batch interval
     val sparkConf = new SparkConf().setAppName("traffic_data")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val ssc = new StreamingContext(sparkConf, Seconds(30))
 
     // Create direct kafka stream with brokers and topics
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
-//    KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet).map(_._2).repartition(1).saveAsTextFiles("test")
-    val inputDStream = ssc.textFileStream("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/data")
-    // Iterate over DStream to get incoming traffic
-    inputDStream.foreachRDD( lines => {
+    KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet).map(_._2)
 
-//      val lines = rdd.map(_._2)
+    // Iterate over DStream to get incoming traffic
+    inputDStream.foreachRDD( rdd => {
+
+      val lines = rdd.map(_._2)
       lines.map( rec => {
         val spl = rec.split(',')
         val len = spl.length
@@ -37,18 +36,11 @@ object TrafficDataStreaming {
         buf.remove(1)
         buf.toArray.mkString(",")
       })
-      lines.saveAsTextFile("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/data_out")
+
+      lines.saveAsTextFile(List("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/", lines.id).mkString(""))
+
     })
 
-//    lines.foreachRDD(rdd => {
-//      rdd.repartition(1)
-//      if(!rdd.isEmpty)
-//        rdd.saveAsTextFile(List(rdd.id, ".test").mkString(""))
-//    })
-//    xformDStream.saveAsTextFiles("test")
-
-    // Start the computation
-//    inputDStream.print(5)
 
     ssc.start()
     ssc.awaitTermination()
