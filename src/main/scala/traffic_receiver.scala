f1bab8fda9ec0964f409c699f1ba9be61015a2d3
Michael Raimi <mar2260@columbia.edu>
2016-09-18 23:21:29 -0400

    Try using foreachRDD

--- src/main/scala/traffic_receiver.scala
+++ src/main/scala/traffic_receiver.scala
@@ -25,7 +25,7 @@ object TrafficDataStreaming {
     val inputDStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
 
     // Iterate over DStream to get incoming traffic
-    val xformDStream = inputDStream.map( rdd => {
+    val xformDStream = inputDStream.foreachRDD( rdd => {
 
       val lines = rdd.map(_._2)
       lines.map( rec => {
