0f0cb68657fb8647ea35205871a4b3eca6e8e530
Michael Raimi <mar2260@columbia.edu>
2016-09-21 00:08:39 -0400

    Calculate distances via RDD and centroids

--- src/main/repl/test_clustering.scala
+++ src/main/repl/test_clustering.scala
@@ -21,3 +21,15 @@ outputDStream.foreachRDD(rdd => {
 })
 
 ssc.start()
+
+/**
+  * This needs to be updated for a streaming context.
+  */
+def distance(a: Vector, b: Vector) =
+  math.sqrt(a.toArray.zip(b.toArray).map(p => p._1 - p._2).map(d => d*d).sum)
+
+def distToCentroid(data: RDD[Vector], model: StreamingKMeansModel) = {
+  /** @val clusters RDD[Int] Centroid indices for each record **/
+  val clusters = data.map(record => (record, model.predict(record)))
+  val distances = clusters.map(tup => (tup._1, distance(tup._1, model.clusterCenters(tup._2))))
+}
