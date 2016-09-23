3377992ae622d813fae4b106b83027cc7eb2854d
Michael Raimi <mar2260@columbia.edu>
2016-09-23 00:45:57 -0400

    Track threshold calculation code

--- src/main/scala/streamingKMeans.scala
+++ src/main/scala/streamingKMeans.scala
@@ -4,7 +4,8 @@ import org.apache.spark.mllib.clustering.StreamingKMeans
 import org.apache.spark.mllib.clustering.StreamingKMeansModel
 import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{Seconds, Streaming Context}
+import scala.collection.Map
 
 object StreamingKMeansExample {
 
@@ -25,10 +26,20 @@ object StreamingKMeansExample {
     clusters.map(tup => (tup._1, distance(tup._1, model.clusterCenters(tup._2))))
   }
 
+  def getThresholds(sc: SparkContext, std_dev_multiplier: Double) = {
+    val thresh = sc.textFile("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/threshold")
+    val tups = thresh.map(line => {
+      val spl = line.split(",")
+      val dub = spl.map(_.toDouble)
+      (dub(0), dub(1)+std_dev_multiplier*dub(2))
+    }).collectAsMap
+  }
+
   def main(args: Array[String]) {
 
     val sparkConf = new SparkConf().setAppName("kmeans")
     val ssc = new StreamingContext(sparkConf, Seconds(5))
+    val thresholds = getThresholds(scc.sparkContext)
 
     val trainingData = ssc.textFileStream("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/train/").map(Vectors.parse)
     val testData = ssc.textFileStream("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/test/").map(Vectors.parse)
@@ -38,6 +49,12 @@ object StreamingKMeansExample {
     model.trainOn(trainingData)
     testData.foreachRDD(rdd => {
       val distRdd = distToCentroid(rdd, model)
+      val results = distRdd.map(distanceTup => {
+        val idx = distanceTup._1
+        val dist = distanceTup._2
+
+
+      })
 
       if (!distRdd.isEmpty){
         /** Write back to the model for updates */
