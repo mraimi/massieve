46f5b4d23e3344dda5689ebaa038bc8c7abc21b5
Michael Raimi <mar2260@columbia.edu>
2016-09-25 18:45:10 -0400

    Track changes wrt Task Not Serializable error

--- src/main/repl/test_clustering.scala
+++ src/main/repl/test_clustering.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.clustering.StreamingKMeans
 import org.apache.spark.mllib.clustering.StreamingKMeansModel
@@ -11,14 +12,16 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 def distance(a: Vector, b: Vector) =
   math.sqrt(a.toArray.zip(b.toArray).map(p => p._1 - p._2).map(d => d*d).sum)
 
+/**
+  * @return Rdd[(Double, Double)]
+  */
 def distToCentroid(data: RDD[Vector], model: StreamingKMeansModel) = {
   val clusters = data.map(record => (record, model.predict(record)))
-  clusters.map(tup => (tup._1, distance(tup._1, model.clusterCenters(tup._2))))
+  clusters.map(tup => (tup._2, distance(tup._1, model.clusterCenters(tup._2))))
 }
 
-def getThresholds(sc: SparkContext, std_dev_multiplier: Double) = {
-  val thresh = sc.textFile("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/stats")
-  thresh.map(line => {
+def getThresholds(stats: RDD[String], std_dev_multiplier: Double) = {
+  stats.map(line => {
     val spl = line.split(",")
     val dub = spl.map(_.toDouble)
     (dub(0), dub(1)+std_dev_multiplier*dub(2))
@@ -26,29 +29,31 @@ def getThresholds(sc: SparkContext, std_dev_multiplier: Double) = {
 }
 
 val ssc = new StreamingContext(sc, Seconds(5))
-
+val sc = ssc.sparkContext
 val trainingData = ssc.textFileStream("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/train/").map(Vectors.parse)
 val testData = ssc.textFileStream("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/test/").map(Vectors.parse)
-val sc = scc.sparkContext
-val thresholds = sc.broadcast(getThresholds(sc, 1.0))
+val statsTextFile = sc.textFile("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/stats")
+val thresholds = getThresholds(statsTextFile, 1.0)
+val bcThresh = sc.broadcast(thresholds)
 val model = new StreamingKMeans().setK(100).setDecayFactor(0).setRandomCenters(38, 0.0)
 
 model.trainOn(trainingData)
-testData.foreachRDD(rdd => {
-  val distRdd = distToCentroid(rdd, model.latestModel)
+val latest = sc.broadcast(model.latestModel)
 
+testData.foreachRDD(rdd => {
+  val distRdd = distToCentroid(rdd, latest.value)
   val results = distRdd.map(distanceTup => {
     val idx = distanceTup._1
     val dist = distanceTup._2
-    if (dist > thresholds(idx)) "Normal" else "Anomalous"
+    if (dist > bcThresh.value(idx)) "Normal" else "Anomalous"
   })
 
   if (!distRdd.isEmpty){
-    distRdd.saveAsObjectFile(List("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/output/distance-", distRdd.id).mkString(""))
+    distRdd.saveAsTextFile(List("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/output/distance-", distRdd.id).mkString(""))
   }
 
   if (!results.isEmpty){
-    results.saveAsObjectFile(List("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/output/traffic-results-", distRdd.id).mkString(""))
+    results.saveAsTextFile(List("hdfs://ec2-23-22-195-205.compute-1.amazonaws.com:9000/output/traffic-results-", distRdd.id).mkString(""))
   }
 })
 
