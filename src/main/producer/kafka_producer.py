736ccb03a9315c1c030d377a3dd508604ec2fdf4
Michael Raimi <mar2260@columbia.edu>
2016-09-29 11:47:42 -0400

    Take limit off of producer

--- src/main/producer/kafka_producer.py
+++ src/main/producer/kafka_producer.py
@@ -17,14 +17,12 @@ class Producer(object):
 
     def produce_msgs(self, source_symbol):
         random.seed()
-        msg_cnt = 0
-        while msg_cnt < 100000:
+        while True:
             idx = random.randint(0, len(self.mem_data) - 1)
             str_fmt = "{}"
             message_content = str_fmt.format(self.mem_data[idx])
             print message_content
             self.producer.send_messages('traffic_data', source_symbol, message_content)
-            msg_cnt += 1
 
 if __name__ == "__main__":
     args = sys.argv
--- src/misc/mise_en_place.sh
+++ src/misc/mise_en_place.sh
@@ -3,12 +3,5 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /test/*
 /usr/local/hadoop/bin/hdfs dfs -rm -r /train/*
 
-#/usr/local/hadoop/bin/hdfs dfs -mv /data/unlabeled_full_numeric /train/
-#/usr/local/hadoop/bin/hdfs dfs -mv /data/unlabeled_test_numeric /test/
-#/usr/local/hadoop/bin/hdfs dfs -copyToLocal /data/unlabeled_full_numeric /home/ubuntu/opt/realtimeAnomalies/src/main/training/
-#/usr/local/hadoop/bin/hdfs dfs -copyToLocal /data/unlabeled_test_numeric /home/ubuntu/opt/realtimeAnomalies/src/main/test/
-
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /home/ubuntu/opt/realtimeAnomalies/src/main/training/unlabeled_full_numeric /intermediary/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /home/ubuntu/opt/realtimeAnomalies/src/main/test/unlabeled_test_numeric /intermediary/
 /usr/local/hadoop/bin/hdfs dfs -mv /intermediary/unlabeled_full_numeric /train/
-/usr/local/hadoop/bin/hdfs dfs -mv /intermediary/unlabeled_test_numeric /test/
