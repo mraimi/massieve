99ce86a1e21dd11943810dfd24b52ab3f09f314d
Michael Raimi <mar2260@columbia.edu>
2016-09-29 18:33:36 +0000

    Use lower threshold to give more varied classifications

--- src/main/scala/traffic_receiver.scala
+++ src/main/scala/traffic_receiver.scala
@@ -63,7 +63,7 @@ object TrafficDataStreaming {
     val inputStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
     val trainingData = ssc.textFileStream(baseUrl + ":9000/train/").map(Vectors.parse)
     val statsTextFile = sc.textFile(baseUrl + ":9000/stats")
-    val thresholds = df.getThresholds(statsTextFile, 2.0)
+    val thresholds = df.getThresholds(statsTextFile, 0.0)
     val bcThresh = sc.broadcast(thresholds)
     val model = new StreamingKMeans().setK(100).setDecayFactor(0.0).setRandomCenters(38, 0.0)
 
